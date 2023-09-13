53583c7461c8d97666427f5910618f712fe1432f
mertcetiin <mrtctn506@gmail.com>
2023-09-13 23:34:43 +0300

    registerHtml made

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
 })
 export class RegisterComponent {
+
+  constructor(private router: Router) { }
+
+  homePage() {
+    this.router.navigate(['/'])
+  }
 
 }
